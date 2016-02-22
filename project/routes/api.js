ffc7d9734d6422e8205ae15b98456592acd9e9cd
WangYingbo <superwyb@gmail.com>
2016-02-22 22:29:27 +0800

    monitor table

--- project/routes/api.js
+++ project/routes/api.js
@@ -2,10 +2,6 @@ var express = require('express');
 var router = express.Router();
 var mysql = require('./mysql').pool;
 
-
-
-
-
 var price_handler = function(monitor, handled, monitors, monitor_list,conn, res){
 	if(monitor === undefined || monitor == null || monitor.type != 'price'){
 		monitor.status = 0;
